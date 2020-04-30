fc68ab5cdf699f604ad30e76733f212b9b440320
Ameya Salankar <salankaras1234@gmail.com>
2020-04-30 13:04:47 +0530

    deleted old files

--- client_app.py
+++
@@ -1,5 +0,0 @@
-from rdt_receive import Client
-
-client = Client("127.0.0.1", 8080, "test.txt")
-client.create_socket()
-client.receive()
--- test4.py
+++
@@ -1,5 +0,0 @@
-from rdt_send import Server
-
-server = Server()
-server.connect()
-server.run()
